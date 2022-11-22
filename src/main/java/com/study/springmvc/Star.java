673f6860da4f960547246c8bb902235d4ca26609
siwonkim <ktldnjs0811@naver.com>
2022-11-23 01:04:15 +0900

    MVC 동작 검증

---
+++ src/main/java/com/study/springmvc/Star.java
@@ -0,0 +1,13 @@
+package com.study.springmvc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Star {
+    String name;
+    int age;
+}
